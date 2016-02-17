1e05b6d74dceec68e19ad887ff0302c4a643a0f0
sinwailam193 <sinwailam193@hotmail.com>
2016-02-17 10:58:30 -0800

    finished setting up searchbar

--- src/containers/search-bar.react.js
+++ src/containers/search-bar.react.js
@@ -3,11 +3,25 @@ import React, {Component} from 'react';
 export default class SearchBar extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      term: ''
+    };
   }
+  _onInputChange = (event) =>{
+    let term = event.target.value;
+    this.setState({term});
+  };
+  _onFormSubmit = (event) =>{
+    // this stops the form from sending stuff to the backend when user hits enter
+    event.preventDefault();
+  };
   render(){
     return (
-      <form className="input-group">
-        <input type="text" />
+      <form onSubmit={this._onFormSubmit} className="input-group">
+        <input type="text" className="form-control" placeholder="Get a five-day forecast in your favorite cities" value={this.state.term} onChange={this._onInputChange} />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
       </form>
     );
   }
