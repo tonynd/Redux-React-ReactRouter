f09f2dcd9884873b3ee8410fdd84ac51d68e54af
sinwailam193 <sinwailam193@hotmail.com>
2016-06-09 16:54:12 -0700

    fixed

--- src/components/app.react.js
+++ src/components/app.react.js
@@ -5,10 +5,11 @@ export default class App extends Component {
   constructor(props){
     super(props);
   }
-  
+
   render(){
     return (
       <div>
+        {this.hello()}
         {this.props.children}
       </div>
     );
