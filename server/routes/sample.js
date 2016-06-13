9d6bfdca434860f6f1f6df7071522f43bb80dc80
sinwailam193 <sinwailam193@hotmail.com>
2016-06-13 10:25:48 -0700

    added comments

--- server/routes/sample.js
+++ server/routes/sample.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+// if there are data send from the frontend via body, use req.body
+// if there are data being send from the frontend via query string like, http://localhost:3000/api/v0/sample/?q=hi
+// use req.query -> this would print {q: "hi"}
+
 router.get('/', (req, res) => {
   res.send({
     data: "Hello World"
